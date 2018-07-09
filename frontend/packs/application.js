edd6f1a32909ac3ddb0ecaa50c472d450c91b674
cmhnk <corinne.m.henk@gmail.com>
2018-07-09 16:41:17 -0700

    create first component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,12 +1,2 @@
-// // frontend/packs/application.js
-// import "./application.css";
-// import "init";
-// // import "components/user/user.js";
-
-// document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
-// console.log('Hello World from Webpacker');
-
-import "./application.css";
-
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
-console.log("yo");
+import "init";
+import "components/page/page";
