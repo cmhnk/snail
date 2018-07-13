5d304fc64fc36f2ad1b945959d5386c67d676ad5
cmhnk <corinne.m.henk@gmail.com>
2018-07-12 20:13:38 -0700

    setup components for address book, users, and user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,25 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  before_action :authenticate!
 
   def hello
     render json: 'hi'
   end
 
   def show
-    @users = User.all
+    @users = User.order(created_at: :asc).last(20)
     # render json: { message: 'Successfully retrieved users.', payload: @users }
   end
 
   def create
     user = User.create(user_params)
     render json: { message: 'New User Created!', payload: user }
+  end
+
+  def private
+
+  # redirect user to /login if hasn't picked a username yet
+  def authenticate!
+    redirect_to login_path unless session[:username]
   end
 
   def user_params
---
+++ frontend/components/address-book/address-book.js
@@ -0,0 +1,3 @@
+import "components/users/users";
+import "components/user-form/user-form";
+import "./address-book.css";
