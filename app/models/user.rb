9baee1f7695b2b4d8ff3bccd27f6e5e6d4bcea2c
cmhnk <corinne.m.henk@gmail.com>
2018-07-17 16:54:35 -0700

    add todo

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  # TODO: this method currently doesn't limit birthdays to only those in the future
+  # need to add additional filtering, ex.
+  # if month is current month, day is not less than current day
   def self.upcoming_birthdays
     current_month = Date.today.month
     next_month = current_month + 1
